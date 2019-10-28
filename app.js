b7bc45f614052f53d124a2cd53ed5cf4ffd0d71f
Lukas Jan <lukasjanonis@gmail.com>
2019-10-28 10:45:20 +0200

    Added compression middleware, updated start script

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -63,11 +64,7 @@ app.use(
   })
 );
 
-// Test middleware
-// app.use((req, res, next) => {
-//   req.requestTime = new Date().toISOString();
-//   next();
-// });
+app.use(compression());
 
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
